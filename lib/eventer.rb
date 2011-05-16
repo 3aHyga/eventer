e3422e173388a2912e4bda6e0da70f6abed1f08f
Малъ Скрылёвъ (Malo Skrylevo) <3aHyga@gmail.com>
2011-05-16 16:08:30 +0400

    Поправленъ и отлаженъ былъ кодъ eventer-а. Переписанъ былъ spec тестовъ

--- lib/eventer.rb
+++ lib/eventer.rb
@@ -1,31 +1,51 @@
 #!/usr/bin/ruby
 
 module Eventer
-  class EventerError < StandardError
+  class UnknownEventError < StandardError
+    def initialize(*args)
+      args[0] = "Unregistered event :#{args[0]}" if args[0]
+      super(*args)
+    end
   end
 
-  def method_missing(sym, *args, &block)
-    if sym.to_s =~ /^on_(.*)/
-      (event, prc) = [ $1.to_sym, block ]
-      raise EventerError.new "Unregistered event :#{event}" unless self.class.events.include? event
-      @__events__ = {} unless @__events__
-      @__events__[event] = [] unless @__events__[event]
-      @__events__[event] << prc unless @__events__[event].include? prc
-    else; super; end
+private
+
+  def add_event(event, &prc)
+    event = event.to_sym
+    @__events__ = {} unless @__events__
+    @__events__[event] = [] unless @__events__[event]
+    @__events__[event] << prc unless @__events__[event].include? prc
+    true
   end
+
+public
 
   def event(event, *args)
     res = {}
 
-    if @__events__ and @__events__[event]
-      @__events__[event].each do |event_h|
-	res[event_h] = event_h.call(args)
+    if @__events__
+      if @__events__[event]
+	@__events__[event].each do |event_h|
+	  res[event_h] = event_h.call(*args)
+	end
+      else
+	[]
       end
     else
-      raise EventerError.new "Unregistered event :#{event}"
+      raise UnknownEventError.new event
     end
 
     res
+  end
+
+  def purge_events(*events)
+    (events.empty? ? @__events__ : events).each do |e|
+	@__events__[e].clear if @__events__[e]
+    end if @__events__
+  end
+
+  def event_rs(event, *args)
+    event(event, *args).map do |x| x[1..-1] end.flatten
   end
 
 end
@@ -33,12 +53,12 @@ end
 
 class Class
 
+public
   def events(*args)
-    return @__events__ if args.empty?
-    return if @__events__
-
-    @__events__ = args.map do |event| event.to_sym end.compact
     include Eventer
+    args.map do |e| e.to_sym end.compact.each do |e|
+      self.class_eval "def on_#{e}(&block); add_event(:#{e}, &block); end"
+    end
   end
 
 end
--- spec/eventer_spec.rb
+++ spec/eventer_spec.rb
@@ -3,60 +3,60 @@
 require File.expand_path('../spec_helper', __FILE__)
 
 describe 'Eventer' do
-    it "Good event match" do
-      class Test
-	events :event
-      end
-
-      t = Test.new
-
-      t.on_event do |args|
-	"Matched"
-      end
-
-      o = (t.event :event).to_a.flatten.map do |x| x.class == String ? x : nil end.compact
-
-      raise "Event result isn't matched" if o != [ "Matched" ]
+  it "Good event match" do
+    class Test
+      events :event
     end
 
-    it "Unknown event triggered" do
-      class Test
-	events :event
-      end
+    t = Test.new
 
-      t = Test.new
-
-      t.on_event do |args|
-	"Matched"
-      end
-
-      o = begin
-	(t.event :unknown).to_a.flatten.map do |x| x.class == String ? x : nil end.compact
-      rescue Eventer::EventerError
-	true
-      end
-
-      raise "Event result isn't matched" if (not o) or (o != true)
+    t.on_event do |args|
+      "Matched"
     end
 
-    it "Try to register an unknown event" do
-      class Test
-	events :event
-      end
+    o = t.event_rs :event
 
-      t = Test.new
+    raise "Event result isn't matched" if o != [ "Matched" ]
+  end
 
-      o = begin
-	t.on_unknown do |args|
-	  "Matched"
-	end
-	false
-      rescue Eventer::EventerError
-	true
-      end
-
-      raise "Event result isn't matched" if (not o) or (o != true)
+  it "Unknown event triggered" do
+    class Test
+      events :event
     end
+
+    t = Test.new
+
+    t.on_event do |args|
+      "Matched"
+    end
+
+    o = begin
+      t.event_rs :unknown
+    rescue Eventer::UnknownEventError
+      true
+    end
+
+    raise "Event result isn't matched" if (not o) or (o != true)
+  end
+
+  it "Try to register an unknown event" do
+    class Test
+      events :event
+    end
+
+    t = Test.new
+
+    o = begin
+      t.on_unknown do |args|
+        "Matched"
+      end
+      false
+    rescue NoMethodError
+      true
+    end
+
+    raise "Event result isn't matched" if (not o) or (o != true)
+  end
 end
 
 
